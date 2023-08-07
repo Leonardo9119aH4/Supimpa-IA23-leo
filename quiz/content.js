b35b4e533468c31c2a4fa847bf023d375e242487
Leonardo9119_aH4 <131017105+Leonardo9119aH4@users.noreply.github.com>
2023-08-07 13:25:57 -0300

    Quiz melhorias

--- quiz/content.js
+++ quiz/content.js
@@ -40,73 +40,76 @@ async function main() {
       Tent = 0
     }
   }
-
+  function Acerto(){
+    Perguntar(++nP)
+    if(!elHard.checked){
+      Po += 1/Er
+      Er = 1
+    }
+    if(elHard.checked){
+      Po++
+    }
+  }
+  function Erro(){
+    T_Er++
+      if(!elHard.checked){
+        Er++
+      }
+      if(elHard.checked){
+        Reset()
+        Tent++
+      }
+  }
+  function Exibir(){
+    if(!elHard.checked){
+      elEr.innerHTML = "Erros: " + T_Er
+    }
+    if(elHard.checked){
+      elEr.innerHTML = "Tentativas: " + Tent
+    }
+    elPo.innerHTML = "Pontuação: " + Po
+  }
   elAlt.addEventListener("click", ev => {
     const AltC = ev.target;
     const aAlt = [...elAlt.children]
     const nAltC = aAlt.indexOf(AltC)
     if (nAltC == quiz[nP].resposta) {
-      Perguntar(++nP)
-      if(!elHard.checked){
-        Po += 1/Er
-        Er = 1
-      }
-      if(elHard.checked){
-        Po++
-      }
+      Acerto()
     }
     else{
-      T_Er++
-      if(!elHard.checked){
-        Er++
-      }
-      if(elHard.checked){
-        Reset()
-        Tent++
-      }
+      Erro()
     }
-    if(!elHard.checked){
-      elEr.innerHTML = "Erros: " + T_Er
-    }
-    if(elHard.checked){
-      elEr.innerHTML = "Tentativas: " + Tent
-    }
-    elPo.innerHTML = "Pontuação: " + Po
+    Exibir()
     Perguntar(nP)
   })
   elNP.addEventListener("click", pev => {
     if(quiz[nP].resposta == "NP"){
-      Perguntar(++nP)
-      if(!elHard.checked){
-        Po += 1/Er
-        Er = 1
-      }
-      if(elHard.checked){
-        Po++
-      }
+      Acerto()
     }
     else{
-      T_Er++
-      if(!elHard.checked){
-        Er++
-      }
-      if(elHard.checked){
-        Reset()
-        Tent++
-      }
+      Erro()
     }
-    if(!elHard.checked){
-      elEr.innerHTML = "Erros: " + T_Er
-    }
-    if(elHard.checked){
-      elEr.innerHTML = "Tentativas: " + Tent
-    }
-    elPo.innerHTML = "Pontuação: " + Po
+    Exibir()
     Perguntar(nP)
   })
+  if(elRP != null){
+    elRP.addEventListener("click", aev =>{
+      if(quiz[nP].resposta == "R"){
+        Acerto()
+      }
+      else{
+        Erro()
+      }
+      Exibir()
+      Perguntar(nP)
+    })
+  }
 
-  //colocar o elRP
-  //colocar RESET
+  elHard.addEventListener("click", reset => {
+    Reset()
+    Exibir()
+    Perguntar(nP)
+  })
 
   Perguntar(0)
 }
